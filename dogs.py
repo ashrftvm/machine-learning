ce6fd95d150a46c5485424f248211f7317077c72
Ashrf Ali <ashrftvm1@gmail.com>
2019-07-29 16:51:19 +0400

    histogram with random nunber generation

--- dogs.py
+++ dogs.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 greyhounds = 500
 labs = 500
 
+# 28 and 24 inches with plus or minus 4 inches by generating random numbers
 grey_height = 28 + 4 * np.random.randn(greyhounds)
 lab_height = 24 + 4 * np.random.randn(labs)
 
